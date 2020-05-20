9a64de8e1414ecb93bc68f72e282a6b6480d8aae
mrwho95 <dnkh903@gmail.com>
2020-05-20 10:35:16 +0800

    add public construct auth

--- app/Http/Controllers/Admin/customerController.php
+++ app/Http/Controllers/Admin/customerController.php
@@ -12,6 +12,10 @@ use App\contactcustomer;
 
 class customerController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
     /**
      * Display a listing of the resource.
      *
