e73b2c00b888b9b7a17d981cbb5596568c0f907a
mrwho95 <dnkh903@gmail.com>
2020-06-01 20:06:01 +0800

    build inbox page

--- app/Http/Controllers/User/inboxController.php
+++ app/Http/Controllers/User/inboxController.php
@@ -4,10 +4,21 @@ namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\customerorder;
+use App\room;
+use Auth;
 
 class inboxController extends Controller
 {
+
+	public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
 	public function index(){
-		return view('user.inbox');
+		$arr['orderData'] = customerorder::where('user_id', Auth::id())->get();
+		$arr['roomData'] = room::all();
+		return view('user.inbox', $arr);
 	}
 }
--- resources/views/user/inbox.blade.php
+++ resources/views/user/inbox.blade.php
@@ -2,5 +2,34 @@
 @section('content')
 <div class="container">
 	<h3>Inbox</h3>
+	@foreach($orderData as $value)
+	<br>
+	<div class="card">
+		<div class="row" style="padding:5%;">
+			<div class="col-lg-4">
+				@foreach($roomData as $key => $data)
+					@if($value->room_id == $data->id)
+                		<img src="{{ asset('uploads/roomPhoto/'.$data->photo_1) }}" alt="room image" class="rounded-circle" style="height: 200px;width: 200px;">
+                	@endif
+                @endforeach  
+			</div>
+			<div class="col-lg-4">
+				<p>{{$value->booking_code}}</p>
+				@foreach($roomData as $key => $data)
+					@if($value->room_id == $data->id)
+                		<strong>{{$data->name}}</strong>
+                	@endif
+                @endforeach 
+				<p>{{$value->check_in}}</p>
+				<p>{{$value->check_out}}</p>
+				<p>{{$value->range}}</p>
+			</div>
+			<div class="col-lg-4 " style="display: flex; justify-content: center; margin-top:50px;">
+				<strong style="font-size: 30pt; text-align: center;">{{$value->status}}</strong>
+			</div>
+		</div>
+	</div>
+	@endforeach
+	<br>
 </div>
 @endsection
